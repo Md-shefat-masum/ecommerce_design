d6d761f0dc295e48e8d852c2ed12a69b7a0e7428
Md-shefat-masum <mshefat924@gmail.com>
2024-03-17 00:11:40 +0600

    js basic learning

---
+++ assets/js/learn.js
@@ -0,0 +1,6 @@
+let name_list = document.querySelector(".name_list");
+let output = document.querySelector(".output");
+
+// output.innerText = name_list.innerText;
+
+// console.log(name_list.innerText);
