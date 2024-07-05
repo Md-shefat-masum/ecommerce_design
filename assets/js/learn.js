e3d61b21fc70d575e3daf41ede0f71676745dbd0
Md-shefat-masum <mshefat924@gmail.com>
2024-07-06 00:44:10 +0600

    js first

--- assets/js/learn.js
+++ assets/js/learn.js
@@ -1,6 +1,38 @@
-let name_list = document.querySelector(".name_list");
-let output = document.querySelector(".output");
 
-// output.innerText = name_list.innerText;
+add.onclick = function () {
+    var num1 = +my_input1.value;
+    var num2 = +my_input2.value;
+    var output = num1 + num2;
 
-// console.log(name_list.innerText);
+    output_box.innerHTML = output;
+}
+
+sub.onclick = function () {
+    var num1 = +my_input1.value;
+    var num2 = +my_input2.value;
+    var output = num1 - num2;
+
+    output_box.innerHTML = output;
+}
+
+mul.onclick = function () {
+    var num1 = +my_input1.value;
+    var num2 = +my_input2.value;
+    var output = num1 * num2;
+
+    output_box.innerHTML = output;
+}
+
+div.onclick = function () {
+    var num1 = +my_input1.value;
+    var num2 = +my_input2.value;
+    var output = num1 / num2;
+
+    output_box.innerHTML = output;
+}
+
+console.log('');
+console.log('');
+console.log('');
+console.log('');
+console.log('');
